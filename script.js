9139fb5e2a7b8db8214acc117a3cefdfa6d139e3
shudong-sheng-sl <shudong.sheng.sl@gmail.com>
2016-09-09 15:24:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ void function(window, document, undefined) {
   "user strict";
 
   var MIN_COLUMN_COUNT = 3; // minimal column count
-  var COLUMN_WIDTH = 220;   // cell width: 190, padding: 14 * 2, border: 1 * 2
+  var COLUMN_WIDTH = 220;   // cell width: 380, padding: 14 * 2, border: 1 * 2
   var CELL_PADDING = 26;    // cell padding: 14 + 10, border: 1 * 2
   var GAP_HEIGHT = 15;      // vertical gap between cells
   var GAP_WIDTH = 15;       // horizontal gap between cells
@@ -153,7 +153,7 @@ void function(window, document, undefined) {
       cell.className = 'cell pending';
       cell.tagLine = 'demo picture ' + key;
       cells.push(cell);
-      front(cellTemplate, { 'title': 'demo picture ' + key, 'src': key, 'height': images[key], 'width': 190 }, cell);
+      front(cellTemplate, { 'title': 'demo picture ' + key, 'src': key, 'height': images[key], 'width': 380 }, cell);
       fragment.appendChild(cell);
     }
     // Faking network latency.
